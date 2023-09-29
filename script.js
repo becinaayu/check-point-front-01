8ae485ccb560939dbc1ae1871055d926302c1db1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 08:36:36 -0300

    Adicionando Checkpoint da Aula

---
+++ script.js
@@ -0,0 +1,33 @@
+let nome = "Beatriz";
+let idade = "18";
+let status = true;
+let valorProduto = 10.50;
+let listaTimes = ["Palmeiras", "Vasco", "Grêmio"];
+
+console.log(nome);
+console.log(idade);
+console.log(status);
+console.log(valorProduto);
+console.log(listaTimes);
+
+// Parte 7
+
+// Variáveis 
+
+let n1 = 2;
+let n2 = 3;
+let n3 = 4;
+let n4 = 5;
+let n5 = 6;
+
+// Operações 
+
+let soma = n1+n2;
+let subtracao = n5-n3;
+let divisao = n4/n1
+let multiplicacao = n3*n4
+
+console.log("O resultado da soma é:", soma);
+console.log("O resultado da subtração é:", subtracao);
+console.log("O resultado da multiplicação é:", multiplicacao);
+console.log("O resultado da divisão é:", divisao);
